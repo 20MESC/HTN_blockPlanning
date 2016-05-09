4820a88b03111a4ba0937085715afa4ac005f42c
Rosario Scalise <romesc@live.com>
2016-05-08 23:22:44 -0400

    5by5 grid with 3 blocks and underspecified line formation WITH one block already in place works

--- tableTopBlocks.py
+++ tableTopBlocks.py
@@ -44,9 +44,9 @@ print("- Define state1:")
 # Create Initial blocks state
 s1 = State('state1')
 # Locations will be in standard graph axes and ordered pairs (x,y)
-s1.locContents = bidict({(1,3):'b1',(2,3):'b2',(3,3):'b3'}) # (1,1) holds b1, (1,2) holds b2
+s1.locContents = bidict({(1,3):'b1',(2,3):'b2',(3,3):'b3',(4,3):'b4',(5,3):'b5'}) # (1,1) holds b1, (1,2) holds b2
 # Could maybe at some point replace this by just checking if the key exists in loc?
-s1.locOccupied = {(x,y):False for x in range(1,4) for y in range(1,4)}
+s1.locOccupied = {(x,y):False for x in range(1,6) for y in range(1,6)}
 s1.locOccupied.update({loc:True for loc in s1.locContents.keys()}) # make sure these reflect the occupied locs
 s1.locRobot = (2,2)
 s1.holding = False 
